972921e46bd0d6566e5cccf9cf89868c14ad295d
Ignacio Vivas <moeshe@gmail.com>
2016-11-24 18:37:30 +0100

    Ejercicios tema 8

--- t4ejer8/src/t4ejer8/t4ejer8.java
+++ t4ejer8/src/t4ejer8/t4ejer8.java
@@ -5,28 +5,28 @@ import java.util.Scanner;
 public class t4ejer8 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner teclado = new Scanner(System.in);
 
 		String frase;
 		int n,i,a, suma;
 		
-
 		System.out.print("Introduce una frase: ");
 		frase=teclado.nextLine();
+		do{
 		System.out.print("Introduce un número: ");
 		a=teclado.nextInt();
-		
+		}while(a<1 || a>10);
 		frase=frase.toUpperCase();
-		System.out.println(frase);
-		
+		System.out.print("La frase codificada es: ");
+
 		for(i=0; i < frase.length(); i++){
 			n=frase.charAt(i);
 			suma=n+a;
-			System.out.println(suma);
-			
+			System.out.print((char) suma);
+
 		}
-		
+
 	}
 
 }
